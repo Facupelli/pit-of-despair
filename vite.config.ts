622fa5a44bd3e563f490b66a62c783a38143a56b
Facundo Personal <facundopellicer4@gmail.com>
2025-07-28 10:38:06 -0300

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -8,7 +8,7 @@ export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "./src"),
+			"@": resolve(__dirname, "./src"),
 		},
 	},
 });
